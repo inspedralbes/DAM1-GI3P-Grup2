631fa36ba1d79f3c13f5eb8650c5d159ae3a17b4
a25abualijab <a25abualijab@inspedralbes.cat>
2026-05-15 10:58:02 +0200

    era vulnerable frente a sql injection ara ja es segur

--- php/guardar_incidencia.php
+++ php/guardar_incidencia.php
@@ -1,7 +1,8 @@
 <?php
 include 'connexio.php';
-//Agafem les dades del formulari
-$departament = $_POST['departament_id'];
+
+// Agafem les dades del formulari
+$departament = (int)$_POST['departament_id'];
 $descripcio = $_POST['descripcio'];
 
 if (empty($descripcio)) {
@@ -10,10 +11,12 @@ if (empty($descripcio)) {
 }
 
 $descripcio = $conn->real_escape_string($descripcio);
-//Comanda per guardar la incidencia a la BD
+
+// Comanda per guardar la incidencia a la BD
 $sql = "INSERT INTO incidencies (descripcio, data_ini, prioritat, departament_id) 
         VALUES ('$descripcio', CURDATE(), 'Baixa', $departament)";
-//Comanda per si hi ha un error o si s'ha guardat correctament
+
+// Comanda per si hi ha un error o si s'ha guardat correctament
 if ($conn->query($sql)) {
     header("Location: registrar_incidencia.php?success=1&id=" . $conn->insert_id);
 } else {
